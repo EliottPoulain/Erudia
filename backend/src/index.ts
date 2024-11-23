9e3d30bbf74946b6360f8190a194eb7760c67e8b
Eliott <eliott.poulain@epitech.eu>
2024-11-23 22:34:42 +0100

    add frontend / backend communication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,16 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 const port: number = 8080;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.get('/', (req: Request, res: Response): void => {
-    res.send('Hello woorld!');
+    res.send('Hello lfjsdflksdjfsdlkfjsdlk!');
 });
 
 app.listen(port, "0.0.0.0", (): void => {
