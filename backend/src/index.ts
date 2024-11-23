64b94ba18986e620498c60bb9b275baab54fcbea
Eliott <eliott.poulain@epitech.eu>
2024-11-23 13:36:15 +0100

    add docker developpement mode for express server

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 8080;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response): void => {
+    res.send('Hello woooofsdlkfslfkj!');
+});
+
+app.listen(port, "0.0.0.0", (): void => {
+    console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
